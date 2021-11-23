e28c2519a726145f6161c65578dcfb4c136ce7df
ella-t <58817053+ella-t@users.noreply.github.com>
2021-11-23 23:24:51 +0000

    EDITOR HAS LOOP

---
+++ Lua/ella-objects.lua
@@ -0,0 +1,17 @@
+
+table.insert(editor_objlist_order, "text_loop")
+
+editor_objlist["text_loop"] = 
+{
+	name = "text_loop",
+	sprite_in_root = false,
+	unittype = "text",
+	tags = {"text_quality","movement"},
+	tiling = -1,
+	type = 2,
+	layer = 20,
+	colour = {1, 2},
+	colour_active = {1, 3},
+}
+
+formatobjlist()
